d3e77ab6ed3349835400c96792d14a189a304aff
pozytiv4ik <pozytiv4ik@gmail.com>
2019-09-30 18:20:24 +0600

    Сделал 2 задание

---
+++ js/stat.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 110;
+var CLOUD_Y = 190;
+var CLOUD_TEXT = 260;
+var GAP = 50;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, 100, 10, '#fff');
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 125, 40);
+  ctx.fillText('Список результатов:', 125, 60);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'hsl(227, 100%, ' + Math.random() * 100 + '%)';
+    }
+    ctx.fillText(names[i], CLOUD_X + GAP * (i + 1) + BAR_WIDTH * i, CLOUD_TEXT);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP * (i + 1) + BAR_WIDTH * i, CLOUD_Y + GAP - 10 - ((BAR_HEIGHT * times[i]) / maxTime));
+    ctx.fillRect(CLOUD_X + GAP * (i + 1) + BAR_WIDTH * i, CLOUD_Y + GAP, BAR_WIDTH, -((BAR_HEIGHT * times[i]) / maxTime));
+  }
+};
