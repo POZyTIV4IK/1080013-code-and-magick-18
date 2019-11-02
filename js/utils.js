4b46f46179b34667c04808408249f430a99dcd24
pozytiv4ik <pozytiv4ik@gmail.com>
2019-11-02 17:20:46 +0600

    Добавил замечания

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,9 @@
 'use strict';
 
 (function () {
+  var getRandomInteger = function (min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  };
   window.utils = {
     WIZARD_FIREBALL: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'],
     WIZARD_NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
@@ -9,8 +12,6 @@
     WIZARD_FAMILYNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
     ESC_KEYCODE: 27,
     ENTER_KEYCODE: 13,
-    getRandomInteger: function (min, max) {
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
+    getRandomInteger: getRandomInteger
   };
 })();
